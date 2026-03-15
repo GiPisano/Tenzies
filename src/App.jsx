35758467c27f61bd7cf9ffcc32e0089b82d27e93
GiPisano <pisanogiacomo9@gmail.com>
2026-03-15 11:07:07 +0100

    Add die element and implement rollDice function

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,30 @@
-export default function App() {
-  return (
-    <main></main>  
-  )
-}
+import { useState } from "react"
+import Die from "./Die"
 
+export default function App() {
+    
+    const [dice, setDice] = useState(generateAllNewDice())
+    
+    function generateAllNewDice() {
+        return new Array(10)
+            .fill(0)
+            .map(() => Math.ceil(Math.random() * 6))
+    }
+    
+    function rollDice() {
+        setDice(generateAllNewDice())
+    }
+    
+    const diceElements = dice.map(num => <Die value={num} />)
+    
+    return (
+        <main>
+            <div className="dice-container">
+                {diceElements}
+            </div>
+            
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
+            
+        </main>
+    )
+}
